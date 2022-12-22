0f49c13bd2186a5224e0f0cb801f2a6ae98b9d15
Puri Virakarin <puri.prv@gmail.com>
2022-12-22 21:56:26 +0700

    Added blog cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { Container, Grid } from '@mantine/core';
 import './App.css'
+import { ArticleCardFooter } from './components/ArticleCard';
+
+
+export interface Forum {
+  id: number;
+  title: string;
+  body: string;
+  date: Date;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    
+    <Container>
+      <Grid >
+      <Grid.Col span={4}>
+        <ArticleCardFooter image={'https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80'} 
+                           category={'Education'} 
+                           title={'National University of Singapore is the best university'} 
+                           footer={'1,233 people liked this'} 
+                           author={{
+                            name: 'Puri Virakarin',
+                            description: 'posted 2 days ago',
+                            image: ''
+        }} />
+      </Grid.Col>
+      <Grid.Col span={4}>
+        <ArticleCardFooter image={'https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80'} 
+                           category={'Education'} 
+                           title={'National University of Singapore is the best university'} 
+                           footer={'1,233 people liked this'} 
+                           author={{
+                            name: 'Puri Virakarin',
+                            description: 'posted 2 days ago',
+                            image: ''
+        }} />
+      </Grid.Col> 
+      <Grid.Col span={4}>
+        <ArticleCardFooter image={'https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80'} 
+                           category={'Education'} 
+                           title={'National University of Singapore is the best university'} 
+                           footer={'1,233 people liked this'} 
+                           author={{
+                            name: 'Puri Virakarin',
+                            description: 'posted 2 days ago',
+                            image: ''
+        }} />
+      </Grid.Col>
+      <Grid.Col span={4}>
+      <ArticleCardFooter image={'https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80'} 
+                           category={'Education'} 
+                           title={'National University of Singapore is the best university'} 
+                           footer={'1,233 people liked this'} 
+                           author={{
+                            name: 'Puri Virakarin',
+                            description: 'posted 2 days ago',
+                            image: ''
+        }} />
+      </Grid.Col>
+      <Grid.Col span={4}>
+      <ArticleCardFooter image={'https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80'} 
+                           category={'Education'} 
+                           title={'National University of Singapore is the best university'} 
+                           footer={'1,233 people liked this'} 
+                           author={{
+                            name: 'Puri Virakarin',
+                            description: 'posted 2 days ago',
+                            image: ''
+        }} />
+      </Grid.Col>
+      </Grid>
+    
+    </Container>
+  );
 }
 
 export default App
---
+++ client/src/utils/theme.tsx
@@ -0,0 +1,14 @@
+import { MantineThemeOverride } from "@mantine/core";
+
+const theme: MantineThemeOverride = {
+    colorScheme: 'light',
+    fontFamily: 'Verdana, sans-serif',
+    fontFamilyMonospace: 'Monaco, Courier, monospace',
+    headings: { fontFamily: 'Greycliff CF, sans-serif' },
+    shadows: {
+        md: '1px 1px 3px rgba(0, 0, 0, .25)',
+        xl: '5px 5px 3px rgba(0, 0, 0, .25)',
+    },
+};
+
+export default theme;
