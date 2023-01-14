892c0a3b4f7c1bb8d3f0cc332c580022dad870c2
Puri Virakarin <puri.prv@gmail.com>
2023-01-14 13:31:38 +0800

    edit structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,23 @@
-import { Container, Grid } from '@mantine/core';
 import './App.css'
-import { ArticleCardFooter } from './components/ArticleCard';
-import { HeaderTabs } from './components/Header';
-import { Col, Menu, Row } from 'antd';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Menu from './pages/Menu'
 import Settings from './pages/Settings';
+import Navbar from './pages/Navbar';
+import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
 
-export interface Forum {
-  id: number;
-  title: string;
-  body: string;
-  date: Date;
-}
 
 function App() {
+  // const { user } = useSelector((state:any) => {
+  //   return {
+  //     user: state.currentUser,
+  //   };
+  // });
+
   return (
-    <>
-      <div>
-        <HeaderTabs user={{
-          name: 'Puri Virakarin',
-          image: 'https://images.unsplash.com/photo-1477554193778-9562c28588c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80'
-        }} tabs={['Home', 'Menu', 'Settings']}  />
-      </ div>
+    <Box>
+      {/* <Navbar user={user}/> */}
       <div className="container">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -30,7 +25,7 @@ function App() {
           <Route path="/settings" element={<Settings />} />
         </Routes>
       </div>
-    </>
+    </Box>
   );
 }
 
